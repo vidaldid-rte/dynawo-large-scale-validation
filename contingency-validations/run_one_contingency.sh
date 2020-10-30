28bd03746c18c2cd33f94f9ad331f317a6253039
Jose Luis Marin <jlmarin@gmail.com>
2020-10-30 17:23:03 +0100

    Use xz compression. Rearrange all saved files into new hierarchy. Shellcheck clean.

--- contingency-validations/run_one_contingency.sh
+++ contingency-validations/run_one_contingency.sh
@@ -10,7 +10,7 @@
 #
 
 # For saner programming:
-set -o nounset -o noclobber
+set -o nounset  # here we don't set noclobber because we need it
 set -o errexit -o pipefail 
 
 
@@ -18,7 +18,7 @@ usage()
 {
     cat <<EOF
 
-Usage: $0 [OPTIONS] INPUTDIR
+Usage: $0 [OPTIONS] BASE_CASE CONTINGENCY_CASE
   Options:
     -c | --cleanup  Delete the input case (both Astre & Dynawo) after getting the results
     -d | --debug    More debug messages
@@ -35,7 +35,7 @@ EOF
 # -use return value from ${PIPESTATUS[0]}, because ! hosed $?
 ! getopt --test > /dev/null 
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    echo "I’m sorry, 'getopt --test' failed in this environment."
     exit 1
 fi
 
@@ -50,6 +50,7 @@ LONGOPTS=cleanup,debug,help,output:,verbose
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     # e.g. return value is 1
     #  then getopt has complained about wrong arguments to stdout
+    usage
     exit 2
 fi
 # read getopt’s output this way to handle the quoting right:
@@ -92,7 +93,7 @@ done
 
 if [ $v = "y" ]; then
     echo "OPTIONS: cleanup: $c, debug: $d, help: $h, outDir: $outDir, verbose: $v"
-    echo "PARAMS: $@"
+    echo "PARAMS: $*"
 fi
 
 if [ $h = "y" ]; then
@@ -101,8 +102,9 @@ if [ $h = "y" ]; then
 fi
 
 # handle non-option arguments
-if [[ $# -ne 1 ]]; then
-    echo -e "\n$0: A single directory name is required."
+if [[ $# -ne 2 ]]; then
+    echo
+    echo "$0: Two directory names are required."
     usage
     exit 4
 fi
@@ -116,77 +118,111 @@ fi
 #######################################
 # The real meat starts here
 ########################################
-inDir=$1   # it could contain whitespace, so remember to quote it!
-prefix=$(basename "$inDir")
-if [ ! -d "$inDir" ]; then
-   echo "Directory $inDir not found."
+BASECASE=$1
+CONTG_CASE=$2
+if [ ! -d "$BASECASE" ]; then
+   echo "ERROR: Basecase $BASECASE not found."
    exit 1
 fi
 
-# Create the output dir if it doesn't exist
-mkdir -p "$outDir"
+if [ ! -d "$CONTG_CASE" ]; then
+   echo "ERROR: Contingency case $CONTG_CASE not found."
+   exit 1
+fi
+prefix=$(basename "$CONTG_CASE")
 
-# Save the PWD to avoid having to deal with absolute/relative paths in outDir vs inDir
+# Create the output dirs if they don't exist
+mkdir -p "$outDir"/csv
+mkdir -p "$outDir"/xml
+mkdir -p "$outDir"/log
+mkdir -p "$outDir"/casediffs
+
+# Save the PWD to avoid having to deal with absolute/relative paths in outDir vs CONTG_CASE
 OLD_PWD=$(pwd)
+
+
+
+#################################################
+# Save the case compactly as diffs from BASECASE
+#################################################
+DIFFS="$outDir"/casediffs/"$prefix"-patch
+if diff -ru "$BASECASE" "$CONTG_CASE" > "$DIFFS"; then
+    echo "ERROR: $BASECASE and $CONTG_CASE are identical."
+    exit 1
+fi
+xz -9f "$DIFFS"
+
+if [ ! -f "$outDir"/casediffs/README ]; then
+    cat <<EOF >"$outDir"/casediffs/README
+    To re-create the contingency case from these patch files:
+
+       1.  cp -a BASECASE CONTG_CASE
+
+       2.  cd CONTG_CASE
+
+       3.  xzcat CONTG_CASE-patch.xz | patch -p1
+
+    And then verify the result with:
+
+       diff -r BASECASE CONTG_CASE
+
+EOF
+fi
+
 
 
 ########################################
 # Run Astre
 ########################################
-if [ ! -d "$inDir"/Astre ]; then
-   echo "Directory $inDir/Astre not found."
+if [ ! -d "$CONTG_CASE"/Astre ]; then
+   echo "Directory $CONTG_CASE/Astre not found."
    exit 1
 fi
-cd "$inDir"/Astre
-rm -f log.txt
-astre donneesModelesEntree.xml > log.txt 2>&1
+cd "$CONTG_CASE"/Astre
+RUNLOG=Astre.runStdout
+astre donneesModelesEntree.xml > "$RUNLOG" 2>&1
 if [ ! -f donneesModelesSortie.csv ]; then
-   echo "Astre run failed. Check the log: $inDir/Astre/log.txt"
+   echo "Astre run failed. Check the runlog: $CONTG_CASE/Astre/$RUNLOG"
    exit 1
 fi
-cd $OLD_PWD
-cp "$inDir"/Astre/donneesModelesSortie.csv "$outDir/$prefix"-Astre.csv
 
-LOG="$outDir/$prefix"-Astre.log
-cp "$inDir"/Astre/log.txt "$LOG"
-ASTRE_LOG="$inDir"/Astre/donneesModelesLog.xml
-if fgrep -q 'level="ERROR"' "$ASTRE_LOG"; then
-    echo -e "\n\n\nSOME ERRORS WERE FOUND -- CONTENT OF donneesModelesLog.xml:" >> "$LOG"
-    cat "$ASTRE_LOG" >> "$LOG"
-fi
+# TODO: launch here the extraction of EVENTS (from the xml to CSV)
+
+# Collect and compress all results
+cd "$OLD_PWD"
+xz -c9 "$CONTG_CASE"/Astre/donneesModelesSortie.csv > "$outDir"/csv/"$prefix"-AstreSortie.csv.xz
+xz -c9 "$CONTG_CASE"/Astre/donneesModelesSortie.xml > "$outDir"/xml/"$prefix"-AstreSortie.xml.xz
+xz -c9 "$CONTG_CASE"/Astre/donneesModelesLog.xml    > "$outDir"/log/"$prefix"-AstreLog.xml.xz
+xz -c9 "$CONTG_CASE"/Astre/"$RUNLOG"                > "$outDir"/log/"$prefix"-"$RUNLOG".xz
+
 
 
 ########################################
 # Run Dynawo
 ########################################
-if [ ! -f "$inDir"/fic_JOB.xml ] || [ ! -d "$inDir"/tFin ]; then
-   echo "Dynawo input files not found under $inDir/."
+if [ ! -f "$CONTG_CASE"/fic_JOB.xml ] || [ ! -d "$CONTG_CASE"/tFin ]; then
+   echo "Dynawo input files not found under $CONTG_CASE/."
    exit 1
 fi
-cd "$inDir"
-rm -f log.txt
-dynawo-RTE jobs fic_JOB.xml > log.txt 2>&1 || true  # allow it to fail while using errexit
+cd "$CONTG_CASE"
+RUNLOG=Dynawo.runStdout
+dynawo-RTE jobs fic_JOB.xml > "$RUNLOG" 2>&1 || true  # allow it to fail while using errexit flag
 if [ ! -f ./tFin/outputs/curves/curves.csv ]; then
-   echo "Dynawo run failed. Check the logs: $inDir/log.txt, $inDir/tFin/outputs/curves/curves.csv"
+   echo "Dynawo run failed. Check Dynawo's log and the run-log: $CONTG_CASE/$RUNLOG"
    exit 1
 fi
-cd $OLD_PWD
-cp "$inDir"/tFin/outputs/curves/curves.csv "$outDir/$prefix"-Dynawo.csv
-cp "$inDir"/tFin/outputs/constraints/constraints.xml "$outDir/$prefix"-Dynawo.constraints.xml
-cp "$inDir"/tFin/outputs/timeLine/timeline.xml "$outDir/$prefix"-Dynawo.timeLine.xml
 
-LOG="$outDir/$prefix"-Dynawo.log
-cp "$inDir"/log.txt "$LOG"
-DYNAWO_LOG="$inDir"/tFin/outputs/logs/dynamo.log
-if fgrep -q 'ERROR' "$DYNAWO_LOG"; then
-    echo -e "\n\n\nSOME ERRORS WERE FOUND -- CONTENT OF dynawo.log:" >> "$LOG"
-    cat "$DYNAWO_LOG" >> "$LOG"
-fi
+# Collect and compress all results
+cd "$OLD_PWD"
+xz -c9 "$CONTG_CASE"/tFin/outputs/curves/curves.csv           > "$outDir"/csv/"$prefix"-Dynawo.csv.xz
+xz -c9 "$CONTG_CASE"/tFin/outputs/constraints/constraints.xml > "$outDir"/xml/"$prefix"-DynawoConstraints.xml.xz
+xz -c9 "$CONTG_CASE"/tFin/outputs/finalState/outputIIDM.xml   > "$outDir"/xml/"$prefix"-DynawoOutputIIDM.xml.xz
+xz -c9 "$CONTG_CASE"/tFin/outputs/timeLine/timeline.xml       > "$outDir"/xml/"$prefix"-DynawoTimeLine.xml.xz
+xz -c9 "$CONTG_CASE"/tFin/outputs/logs/dynamo.log             > "$outDir"/log/"$prefix"-Dynawo.log.xz
+xz -c9 "$CONTG_CASE"/"$RUNLOG"                                > "$outDir"/log/"$prefix"-"$RUNLOG".xz
 
-
-
-# Delete input dir if cleanup
+# Delete input dir if cleanup was requested
 if [ $c = "y" ]; then
-    rm -rf "$inDir"
+    rm -rf "$CONTG_CASE"
 fi
 
