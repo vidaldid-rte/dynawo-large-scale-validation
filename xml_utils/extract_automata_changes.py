2d9197e2c04b30a2fba3427ce22c14ae5ac90939
Jose Luis Marin <jlmarin@gmail.com>
2020-11-09 09:44:16 +0100

    Extract load-transformer tap changing events, too

--- xml_utils/extract_automata_changes.py
+++ xml_utils/extract_automata_changes.py
@@ -52,6 +52,7 @@ import sys
 import os
 from lxml import etree
 import pandas as pd
+from collections import namedtuple
 
 
 DYD_FILE = "/tFin/fic_DYD.xml"
@@ -59,6 +60,12 @@ ASTRE_EVENTS_IN = "/Astre/donneesModelesSortie.xml"
 ASTRE_EVENTS_OUT = "/Astre/Astre_automata_changes.csv"
 DYNAWO_EVENTS_IN = "/tFin/outputs/timeLine/timeline.xml"
 DYNAWO_EVENTS_OUT = "/Dynawo_automata_changes.csv"
+
+devtype_xfmer = "Transformer"
+devtype_loadxfmer = "Load_Transformer"
+devtype_shunt = "Shunt"
+devtype_shuntctrl = "Shunt_Control"
+devtype_klevel = "K_level"
 
 verbose = False
 
@@ -129,31 +136,36 @@ def extract_astre_events(astre_input):
     for event in root.iter("{%s}evtchronologie" % ns):
         # Transformer taps
         if event.get("type") == "9" and event.get("evenement") == "1":  # PRISEPLUS1
-            append_astre_data(data, event, "TRANSFORMER", "TapUp")
+            append_astre_data(data, event, devtype_xfmer, "TapUp")
         elif event.get("type") == "9" and event.get("evenement") == "2":  # PRISEMOINS1
-            append_astre_data(data, event, "TRANSFORMER", "TapDown")
+            append_astre_data(data, event, devtype_xfmer, "TapDown")
+        # Load-Transformer taps
+        if event.get("type") == "7" and event.get("evenement") == "1":  # PRISEPLUS1
+            append_astre_data(data, event, devtype_loadxfmer, "TapUp")
+        elif event.get("type") == "7" and event.get("evenement") == "2":  # PRISEMOINS1
+            append_astre_data(data, event, devtype_loadxfmer, "TapDown")
         # Shunts
         elif (
             event.get("type") == "4" and event.get("evenement") == "21"
         ):  # ACMC_ENCLENCHEMENT
-            append_astre_data(data, event, "SHUNT", "ShuntConnected")
+            append_astre_data(data, event, devtype_shunt, "ShuntConnected")
         elif (
             event.get("type") == "4" and event.get("evenement") == "22"
         ):  # ACMC_DECLENCHEMENT
-            append_astre_data(data, event, "SHUNT", "ShuntDisconnected")
+            append_astre_data(data, event, devtype_shunt, "ShuntDisconnected")
         elif (
             event.get("type") == "4" and event.get("evenement") == "33"
         ):  # SMACC_ENCLENCHEMENT
-            append_astre_data(data, event, "SHUNT", "ShuntConnected")
+            append_astre_data(data, event, devtype_shunt, "ShuntConnected")
         elif (
             event.get("type") == "4" and event.get("evenement") == "34"
         ):  # SMACC_DECLENCHEMENT
-            append_astre_data(data, event, "SHUNT", "ShuntDisconnected")
+            append_astre_data(data, event, devtype_shunt, "ShuntDisconnected")
         # K-levels
         elif (
             event.get("type") == "2" and event.get("evenement") == "18"
         ):  # RST_CONSIGNE
-            append_astre_data(data, event, "SVC_Klevel", "NewRstLevel")
+            append_astre_data(data, event, devtype_klevel, "NewRstLevel")
 
     # Translate the device IDs to their names
     astre_id2name(data, root)
@@ -183,6 +195,10 @@ def astre_id2name(data, tree_root):
     for xfmer in tree_root.iter("{%s}quadripole" % ns):
         xfmer_names[xfmer.get("num")] = xfmer.get("nom")
 
+    loadxfmer_names = dict()
+    for loadxfmer in tree_root.iter("{%s}conso" % ns):
+        loadxfmer_names[loadxfmer.get("num")] = loadxfmer.get("nom")
+
     shunt_names = dict()
     for shunt in tree_root.iter("{%s}shunt" % ns):
         shunt_names[shunt.get("num")] = shunt.get("nom")
@@ -194,11 +210,13 @@ def astre_id2name(data, tree_root):
     # Now depending on device type (row[0]), translate the ouvrage id
     # to its name (row[1])
     for row in data:
-        if row[0] == "TRANSFORMER":
+        if row[0] == devtype_xfmer:
             row[1] = xfmer_names.get(row[1], "**ERROR***")
-        elif row[0] == "SHUNT":
+        elif row[0] == devtype_loadxfmer:
+            row[1] = loadxfmer_names.get(row[1], "**ERROR***")
+        elif row[0] == devtype_shunt:
             row[1] = shunt_names.get(row[1], "**ERROR***")
-        elif row[0] == "SVC_Klevel":
+        elif row[0] == devtype_klevel:
             row[1] = klevel_names.get(row[1], "**ERROR***")
 
 
@@ -213,28 +231,28 @@ def extract_dynawo_events(dynawo_input, dynawo_dyd):
 
     # We enumerate all events and extract the types we need
     for event in root.iter("{%s}event" % ns):
-        # Transformer taps
+        # Transformer and Load-Transformer taps
         if event.get("message") == "Tap +1":
-            append_dynawo_data(data, event, "TRANSFORMER", "TapUp")
+            append_dynawo_data(data, event, devtype_xfmer, "TapUp")
         elif event.get("message") == "Tap -1":
-            append_dynawo_data(data, event, "TRANSFORMER", "TapDown")
+            append_dynawo_data(data, event, devtype_xfmer, "TapDown")
         # Shunts
         elif event.get("message") == "SHUNT : connecting":
-            append_dynawo_data(data, event, "SHUNT", "ShuntConnected")
+            append_dynawo_data(data, event, devtype_shunt, "ShuntConnected")
         elif event.get("message") == "SHUNT : disconnecting":
-            append_dynawo_data(data, event, "SHUNT", "ShuntDisconnected")
+            append_dynawo_data(data, event, devtype_shunt, "ShuntDisconnected")
         elif event.get("message")[:19] == "VCS : shunt number ":
             if event.get("message")[-8:] == " closing":
-                append_dynawo_data(data, event, "SHUNT", "AcmcShuntClosing")
+                append_dynawo_data(data, event, devtype_shuntctrl, "AcmcShuntClosing")
             elif event.get("message")[-8:] == " opening":
-                append_dynawo_data(data, event, "SHUNT", "AcmcShuntOpening")
+                append_dynawo_data(data, event, devtype_shuntctrl, "AcmcShuntOpening")
         elif event.get("message")[:28] == "MVCS : closing shunt number ":
-            append_dynawo_data(data, event, "SHUNT", "SmaccClosingDelayPast")
+            append_dynawo_data(data, event, devtype_shuntctrl, "SmaccClosingDelayPast")
         elif event.get("message")[:28] == "MVCS : opening shunt number ":
-            append_dynawo_data(data, event, "SHUNT", "SmaccOpeningDelayPast")
+            append_dynawo_data(data, event, devtype_shuntctrl, "SmaccOpeningDelayPast")
         # K-levels
         elif event.get("message")[:21] == "SVC Area : new level ":
-            append_dynawo_data(data, event, "SVC_Klevel", "NewRstLevel")
+            append_dynawo_data(data, event, devtype_klevel, "NewRstLevel")
 
     # Translate the dynamic model labels to their static device counterparts
     dynawo_id2name(data, dynawo_dyd)
@@ -263,10 +281,13 @@ def dynawo_id2name(data, dynawo_dyd):
 
     # Build a dict: "Dynamic model name" ==> "Static name" (one for ALL types)
     dm_names = dict()
+    Dev_info = namedtuple("Dev_info", ["lib_name", "static_id"])
     for dm in root.iter("{%s}blackBoxModel" % ns):
-        static_name = dm.get("staticId")
-        if static_name is not None:
-            dm_names[dm.get("id")] = [dm.get("lib"), static_name]
+        static_id = dm.get("staticId")
+        if static_id is not None:
+            dm_names[dm.get("id")] = Dev_info(
+                lib_name=dm.get("lib"), static_id=static_id
+            )
 
     # Now, depending on the device type (row[0]), translate the
     # dynamic model id (row[1]) to its static name
@@ -274,18 +295,26 @@ def dynawo_id2name(data, dynawo_dyd):
         # It seems that many (all?) TapUp/TapDown messages coming from
         # Dynamic Models are actually load-transformers, not
         # transmission transformers. So we'll also change the device
-        # type to its ddb model library name, to tell them apart in
-        # the output.
-        if row[0] == "TRANSFORMER" and row[1][:3] == "DM_":
-            row[0] = dm_names.get(row[1], ["**ERROR***", "**ERROR***"])[0]
-            row[1] = dm_names.get(row[1], ["**ERROR***", "**ERROR***"])[1]
+        # type to its ddb model library name, to tell them apart.
+        if row[0] == devtype_xfmer and row[1][:3] == "DM_":
+            dm_libname = dm_names.get(row[1], ["ERROR", "ERROR"]).lib_name
+            row[1] = dm_names.get(row[1], ["ERROR", "ERROR"]).static_id
+            if dm_libname[:4] == "Load":
+                row[0] = devtype_loadxfmer
+            elif dm_libname[:11] != "Transformer":
+                row[0] = dm_libname
 
 
 def save_extracted_events(df_ast, df_dwo, astre_output, dynawo_output):
 
-    # Filter events. Use Panda's query() syntax (use None for no filter)
-    # evt_filter = 'DEVICE_TYPE in ["TRANSFORMER", "SHUNT", "SVC_Klevel"]'
-    evt_filter = 'DEVICE_TYPE in ["TRANSFORMER", "SHUNT"]'
+    # Filter events. Use Panda's query() syntax (use None for no filter).
+    # evt_filter = "DEVICE_TYPE in ['%s', '%s', '%s']" % (devtype_xfmer,
+    # devtype_loadxfmer, devtype_shunt)
+    evt_filter = "DEVICE_TYPE in ['%s', '%s', '%s']" % (
+        devtype_xfmer,
+        devtype_loadxfmer,
+        devtype_shunt,
+    )
     if evt_filter is not None:
         df_ast = df_ast.query(evt_filter)
         df_dwo = df_dwo.query(evt_filter)
