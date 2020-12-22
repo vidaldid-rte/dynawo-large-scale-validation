94ce4dcce4f1614a4731edec87d481f2fde9e918
Jose Luis Marin <jlmarin@gmail.com>
2020-12-22 13:40:22 +0100

    Allow passing patterns. Handle paths in a cross-platform way.

--- graph_utils/generate_notebooks.py
+++ graph_utils/generate_notebooks.py
@@ -7,37 +7,53 @@
 #
 #
 # generate_notebooks.py: a simple script to generate a Notebook per CASE. You only
-# need to provide the base directory that contains the case results.
+# need to provide the base directory that contains the cases that have been run.
 #
 
 import sys
 import os
 import nbformat as nbf
-# import glob
 from pathlib import Path
 
-# You only need to edit this (use the "/" operator to be cross-platform)
-BASE_DIR = Path.home() / "work"
 
-# And, typically, you won't need to change these two
-RESULTS_PATTERN = "*/*.FALCON_RESULTS"
+# Default list of contingency types
 DEV_PREFIXES = {"gens": "gen_", "loads": "load_", "shunts": "shunt_"}
 
 
 def main():
-    # Scan the work directory for Results dirs
+
+    # Default pattern for case results directories
+    RESULTS_PATTERN = "*/*.RESULTS"
+
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("\nUsage: %s BASE_DIR ['RESULTS_PATTERN']" % sys.argv[0])
+        print("(default RESULTS_PATTERN = '%s'" % RESULTS_PATTERN)
+        print(
+            "\nExample:\n   %s %s 'PtFige.*/*.RESULTS'"
+            % (sys.argv[0], str(Path.home() / "work"))
+        )
+        print("   (enclose the pattern in quotes)")
+        return 2
+    BASE_DIR = Path(sys.argv[1])
+    if len(sys.argv) > 2:
+        RESULTS_PATTERN = sys.argv[2]
+    else:
+        print("   Using default RESULTS_PATTERN = '%s'" % RESULTS_PATTERN)
+
+    # Scan the work directory for results directories
     dirlist = BASE_DIR.glob(RESULTS_PATTERN)
 
-    # Read master Notebook
+    # Read the master template
     nb = nbf.read("Dynawo-Astre Comparison.template.ipynb", as_version=4)
-    # Generate them
+    # Generate the notebooks
     for results_dir in dirlist:
         for device in DEV_PREFIXES:
             src = (
                 "# Case selection (adapt CRV_DIR as needed)\n"
-                + "CRV_DIR = '%s/%s/crv'\n" % (results_dir, device)
+                + "CRV_DIR = '%s'\n" % str(results_dir / device / "crv")
                 + "PREFIX = '%s'" % DEV_PREFIXES[device]
             )
+            # rewrite the first cell
             nb["cells"][0]["source"] = src
             name = os.path.normpath(results_dir).split(os.sep)
             filename = name[-2] + "_" + name[-1] + "_" + device + ".ipynb"
